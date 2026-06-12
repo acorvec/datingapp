5b4356d6e04d34acdb78c9ffb7a1f52a30c080ab
Andrew Corvec <andrew@corvec.org>
2026-06-12 00:32:40 -0500

    fixed 404 error

--- src/appCalls.js
+++ src/appCalls.js
@@ -3,6 +3,18 @@ const express = require("express");
 
 const views = require("./views.js");
 
+function isolateEndpoint(path) {
+    const splitPath = path.split("/");
+    const lastItem = splitPath[splitPath.length - 1];
+    let usedLength = 0;
+    if (lastItem === "") usedLength = splitPath.length - 1;
+    else usedLength = splitPath.length;
+    if (usedLength === 3) return splitPath[2];
+    else if (usedLength === 2) return "/";
+    else if (usedLength === 1) return "/";
+    else return undefined;
+}
+
 module.exports = {
     setCalls: (app) => {
         app.set("views", "./views");
@@ -13,9 +25,9 @@ module.exports = {
         app.use(express.static(publicDir));
 
         const getAuthorFilename = (request) => {
-            let authorFileName = request.params.name;
-            if (!authorFileName)
-                authorFileName = 'andrew';
+            let authorFileName = 'andrew';
+            if (request.params && request.params.name) 
+                authorFileName = request.params.name;
 
             return { authorFileName: authorFileName };
         };
@@ -34,8 +46,19 @@ module.exports = {
             const obj = getAuthorFilename(request);
             views.showOthers(response, obj['authorFileName'], next);
         });
-        
+
         app.get('/:name/', indexFn);
-        app.get('/', indexFn);
+
+        app.get('*', (request, response, next) => {
+            const endpoint = isolateEndpoint(request.url);
+            if (endpoint === '/') {
+                const obj = getAuthorFilename(request);
+                indexFn(request, response, next);
+            }
+            else {
+                const message = `404: page not found "${request.url}".`;
+                views.showErr(response, message);
+            }
+        });
     },
 };
